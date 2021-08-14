380c4722c2a44cec9331e517160e8d738a1364ac
nithin <kadapallanithin@gmail.com>
2021-08-14 19:58:01 +0530

    added mixed data

--- weed/classwise/prepare_dataset.py
+++ weed/classwise/prepare_dataset.py
@@ -1,6 +1,6 @@
 import os 
 target_dir = "../dataset"
-
+classwise_dir = "."
 #try:
 if not os.path.exists(target_dir):
     os.mkdir(target_dir)
@@ -8,19 +8,24 @@ if not os.path.exists(target_dir):
 images_dir = os.path.join(target_dir,"images")
 labels_dir = os.path.join(target_dir,"labels")
 images_train_dir = os.path.join(images_dir,"train")
+images_test_dir = os.path.join(images_dir,"test")
 labels_train_dir = os.path.join(labels_dir,"train")
+labels_test_dir = os.path.join(labels_dir,"test")
 os.mkdir(images_dir)
 os.mkdir(labels_dir)
 os.mkdir(images_train_dir)
+os.mkdir(images_test_dir)
 os.mkdir(labels_train_dir)
+os.mkdir(labels_test_dir)
 
-classes = []
-class_index = -1
-for dir in os.listdir('.'):
+classes = ["hizda","bendalam","odipili","erra","gunjara","jiluga","jonna","x","paddy","a","janumu] #[]
+# class_index = -1
+os.chdir(classwise_dir)
+for dir in classes:#os.listdir('.'):
     if os.path.isdir(dir):
         # if not f in classes:
-        classes.append(dir)
-        class_index += 1
+        # classes.append(dir)
+        # class_index += 1
         for img in os.listdir(dir):
             if img.endswith(".jpg"):
                 img_path = os.path.join(dir,img)
@@ -35,7 +40,9 @@ for dir in os.listdir('.'):
                     os.system(f"cp {img_path} {target_img_path}")
                 else:
                     print(f"Could not find labels for {dir}/{img}")
-f = open(os.path.join(labels_train_dir,"classes.txt"),'w')
-for c in classes:
-    f.write(c+"\n")
-f.close()
+# f = open(os.path.join(labels_train_dir,"classes.txt"),'w')
+# for c in classes:
+#     f.write(c+"\n")
+# f.close()
+with open('custom.yaml','w') as f:
+    f.write("train: "+images_train_dir+"\nval:"+images_test_dir+"\nnc: "+str(len(classes))+"\nclasses:"+str(classes))
